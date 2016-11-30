016403cf2408ffee439335d3276c62e9ba4c0e7a
Gabe Wyatt <gwyattkelsey@gmail.com>
2016-11-30 00:20:44 -0800

    Added `SyntaxError` and `SecurityError`.

---
+++ Exceptions/Ruby/ScriptError/SyntaxError/code.rb
@@ -0,0 +1,47 @@
+# 1
+def print_exception(exception, explicit)
+    puts "[#{explicit ? 'EXPLICIT' : 'INEXPLICIT'}] #{exception.class}: #{exception.message}"
+    puts exception.backtrace.join("\n")
+end
+
+begin
+    1=2
+rescue SyntaxError => e
+    print_exception(e, true)
+end
+
+# 2
+def print_exception(exception, explicit)
+    puts "[#{explicit ? 'EXPLICIT' : 'INEXPLICIT'}] #{exception.class}: #{exception.message}"
+    puts exception.backtrace.join("\n")
+end
+
+begin
+    require_relative "invalid"
+rescue SyntaxError => e
+    print_exception(e, true)
+end
+
+# 3
+def print_exception(exception, explicit)
+    puts "[#{explicit ? 'EXPLICIT' : 'INEXPLICIT'}] #{exception.class}: #{exception.message}"
+    puts exception.backtrace.join("\n")
+end
+
+begin
+    eval("1=2")
+rescue SyntaxError => e
+    print_exception(e, true)
+end
+
+# 4
+def print_exception(exception, explicit)
+    puts "[#{explicit ? 'EXPLICIT' : 'INEXPLICIT'}] #{exception.class}: #{exception.message}"
+    puts exception.backtrace.join("\n")
+end
+
+begin
+    eval("1=2")
+rescue => e
+    print_exception(e, false)
+end
---
+++ Exceptions/Ruby/ScriptError/SyntaxError/invalid.rb
@@ -0,0 +1,2 @@
+# invalid.rb
+1=2
