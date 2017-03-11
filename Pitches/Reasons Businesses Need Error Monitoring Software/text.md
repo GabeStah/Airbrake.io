28ad40e7a05a211054f838ae989883d5fea41166
Gabe Wyatt <gwyattkelsey@gmail.com>
2017-03-11 00:27:48 -0800

    Added `Service` test solution.

---
+++ Pitches/Reasons Businesses Need Error Monitoring Software/text.md
@@ -0,0 +1,9 @@
+# Reasons Businesses Need Error Monitoring Software
+
+> https://www.quora.com/What-are-some-of-the-best-cloud-based-error-monitoring-reporting-tools-e-g-Airbrake-io
+
+---
+
+__SOURCES__
+
+- 
---
+++ Service/Integration/Integration/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SharpBrake;
+
+namespace Integration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParseExample();
+        }
+
+        static void ParseExample()
+        {
+            string[] values = { "123", "-123", "123.0", "01AD" };
+
+            foreach (var value in values)
+            {
+                Log($"TryParse of {value} is: {Int32.TryParse(value, out int number)}");
+            }
+
+            try
+            {
+                foreach (var value in values)
+                {
+                    Log($"Parse of {value} is: {Int32.Parse(value)}");
+                }
+            }
+            catch (System.FormatException e)
+            {
+                Log(e);
+                e.SendToAirbrake();
+            }
+        }
+
+        static void Log(object value)
+        {
+            #if DEBUG
+                System.Diagnostics.Debug.WriteLine(value);
+            #else
+                Console.WriteLine(value);
+            #endif
+        }
+    }
+}
